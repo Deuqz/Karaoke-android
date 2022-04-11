007fce36d237ea5f43822a6f09ab05e89379f0e3
Deuqz <ddemon2002@mail.ru>
2022-04-12 00:10:05 +0300

    Create project

--- app/src/main/java/com/example/karaoke_android/MainActivity.java
+++ app/src/main/java/com/example/karaoke_android/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.karaoke_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import database.Track;
+import voice.TrackPlayer;
+import voice.TrackPlayerSimple;
+
 public class MainActivity extends AppCompatActivity {
+    TrackPlayer trackPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        trackPlayer = new TrackPlayerSimple(getApplicationContext());
+    }
+
+    // code below from Denis
+    private boolean pausePushed = false;
+    public void playTrackPushed(View view) {
+        if (!pausePushed) {
+            trackPlayer.setTrack(new Track("track1", "somebody", R.raw.track1));
+        }
+        trackPlayer.play();
+    }
+
+    public void pauseTrackPushed(View view) {
+        trackPlayer.pause();
+        pausePushed = true;
+    }
+
+    public void recordPushed(View view) {
+
+    }
+
+    public void stopRecordPushed(View view) {
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        trackPlayer.stop();
     }
 }
---
+++ app/src/main/java/database/Track.java
@@ -0,0 +1,13 @@
+package database;
+
+public class Track {
+    public String name;
+    public String author;
+    public int id;
+
+    public Track(String name, String author, int id) {
+        this.name = name;
+        this.author = author;
+        this.id = id;
+    }
+}
