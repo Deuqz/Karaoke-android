f603c02b26ef6310654e032afa086f0e11b68283
arkady <arksap2002@gmail.com>
2022-04-18 14:13:06 +0300

    tabs

--- app/src/main/java/com/example/karaoke_android/MusicFragment.java
+++ app/src/main/java/com/example/karaoke_android/MusicFragment.java
@@ -1,20 +1,28 @@
 package com.example.karaoke_android;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 public class MusicFragment extends Fragment {
 
+    public MusicFragment() {
+// Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+// Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_music, container, false);
     }
+
 }
