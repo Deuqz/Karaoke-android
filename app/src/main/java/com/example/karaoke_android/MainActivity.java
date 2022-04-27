be9d310d82113bc2f9e58a3cea3150cb2d461e47
arkady <arksap2002@gmail.com>
2022-04-27 18:34:52 +0300

    fix fragment start

--- app/src/main/java/com/example/karaoke_android/MainActivity.java
+++ app/src/main/java/com/example/karaoke_android/MainActivity.java
@@ -35,40 +35,52 @@ public class MainActivity extends AppCompatActivity {
         TabLayout.Tab profileTab = tabLayout.newTab();
         profileTab.setText("\uD83D\uDC64");
         tabLayout.addTab(profileTab);
+        User user = getIntent().getParcelableExtra("User");
+        Fragment fragment = MusicFragment.newInstance(user);
+        startFragment(fragment);
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                Fragment fragment = null;
-                User user = getIntent().getParcelableExtra("User");
-                switch (tab.getPosition()) {
-                    case 0:
-                        fragment = MusicFragment.newInstance(user);
-                        break;
-                    case 1:
-                        fragment = new SearchFragment();
-                        break;
-                    case 2:
-                        fragment = new SettingsFragment();
-                        break;
-                    case 3:
-                        fragment = ProfileFragment.newInstance(user);
-                        break;
-                }
-                FragmentManager fm = getSupportFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                assert fragment != null;
-                ft.replace(R.id.frameLayout, fragment);
-                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-                ft.commit();
+                startFragment(getFragment(tab, user));
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
+                startFragment(getFragment(tab, user));
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
+                startFragment(getFragment(tab, user));
             }
         });
+    }
+
+    private void startFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        assert fragment != null;
+        ft.replace(R.id.frameLayout, fragment);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.commit();
+    }
+
+    private Fragment getFragment(TabLayout.Tab tab, User user) {
+        Fragment fragment = null;
+        switch (tab.getPosition()) {
+            case 0:
+                fragment = MusicFragment.newInstance(user);
+                break;
+            case 1:
+                fragment = new SearchFragment();
+                break;
+            case 2:
+                fragment = new SettingsFragment();
+                break;
+            case 3:
+                fragment = ProfileFragment.newInstance(user);
+                break;
+        }
+        return fragment;
     }
 }
